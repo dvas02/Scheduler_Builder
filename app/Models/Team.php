11ba70e772efe8b37c3e6d5c1849fd5bee00e638
Dov Vas <dov.vas02@gmail.com>
2025-03-18 20:26:45 -0400

    Added divisions to teams

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -7,28 +7,29 @@ use Illuminate\Database\Eloquent\Model;
 class Team
 {
     private static $teams = [
-        [1, 'Lions'],
-        [2, 'Tigers'],
-        [3, 'Bears'],
-        [4, 'Hawks'],
-        [5, 'Eagles'],
-        [6, 'Wolves'],
-        [7, 'Alouettes'],
-        [8, 'Patriots'],
-        [9, 'Chiefs'],
-        [10, 'Titans'],
-        [11, 'Heat'],
-        [12, 'Twelve'],
-        [13, 'Thirteen'],
-        [14, 'Fourteen'],
-        [15, 'Fifteen'],
-        [16, 'Sixteen'],
-        [17, 'Seventeen'],
-        [18, 'Eighteen'],
-        [19, 'Nineteen'],
-        [20, 'Twenty'],
-        [21, 'Twenty One'],
-        [22, 'Twenty Two']
+        // team = [team_id, team_name, team_div]
+        [1, 'Lions', 1],
+        [2, 'Tigers', 2],
+        [3, 'Bears', 1],
+        [4, 'Hawks', 2],
+        [5, 'Eagles', 1],
+        [6, 'Wolves', 2],
+        [7, 'Alouettes', 1],
+        [8, 'Patriots', 2],
+        [9, 'Chiefs', 1],
+        [10, 'Titans', 2],
+        [11, 'Heat', 1],
+        [12, 'Twelve', 2],
+        [13, 'Thirteen', 1],
+        [14, 'Fourteen', 2],
+        [15, 'Fifteen', 1],
+        [16, 'Sixteen', 2],
+        [17, 'Seventeen', 1],
+        [18, 'Eighteen', 2],
+        [19, 'Nineteen', 1],
+        [20, 'Twenty', 2],
+        [21, 'Twenty One', 1],
+        [22, 'Twenty Two', 2]
     ];
 
     public static function all()
